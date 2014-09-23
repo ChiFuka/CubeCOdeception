a005df1431db02473c44b71ec658b81635e2dbc5
MemoariChiFuka <kovanyov@gmail.com>
2014-09-23 19:22:30 +0300

    Login.

--- tests/acceptance/loginCept.php
+++ tests/acceptance/loginCept.php
@@ -1,15 +1,18 @@
 <?php 
+$username = 'qa_user';
+$userpass = '1234567890';
 $I = new AcceptanceTester($scenario);
 $I->wantTo('Log in');
 $I->amOnPage('/');
 $I->see('LOGIN NOW');
-$I->fillField('singup_email', 'qa_user');
-$I->fillField('Password', '21234567890');
-$I->click('Log In');
-$I->amOnPage('/research-intro-page-1/');
-#$I->canSeeInCurrentUrl('livewall');
-#$I->see('MyWall');
-#$I->click('//*[@title="MyWall"]');
-#
-#
-#$I->canSeeInCurrentUrl('research-intro-page-1');
+$I->fillField('#user_login', 'qa_admin');
+$I->fillField('#user_pass', '1234567890');
+$I->click('//p[3]/button');
+$I->waitForElementNotVisible('#agree_button', 5);
+$I->seeCurrentUrlEquals('/research-intro-page-1/');
+$I->click('.holder');
+$I->seeCurrentUrlEquals('/research-intro-page-2/');
+$I->click('.holder');
+$I->seeCurrentUrlEquals('/research-intro-page-3/');
+$I->click('.holder');
+$I->seeCurrentUrlEquals('/members/qa_user/mywall/');
